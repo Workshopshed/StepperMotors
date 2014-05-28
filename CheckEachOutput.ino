ac05f4983e14ea85694cedd392135345bc122f3c
Andy <andy@workshopshed.com>
2014-05-28 22:51:13 +0100

    Update CheckEachOutput.ino
    Fixed issue where board had pin 8 and 9 shorted together

--- CheckEachOutput.ino
+++ CheckEachOutput.ino
@@ -4,7 +4,7 @@ void setup() {
 pinMode(6, OUTPUT); 
 pinMode(7, OUTPUT); 
 pinMode(8, OUTPUT); 
-pinMode(9, OUTPUT); 
+pinMode(20, OUTPUT);  //remapped from pin 9 as that was shorted to pin 8
 pinMode(10, OUTPUT); 
 pinMode(14, OUTPUT); 
 pinMode(15, OUTPUT); 
@@ -14,14 +14,16 @@ Serial.begin(9600);
  
 // the loop routine runs over and over again forever:
 void loop() {
+
 FlashPin(6); 
 FlashPin(7); 
 FlashPin(8); 
-FlashPin(9); 
-FlashPin(10); 
-FlashPin(14); 
+FlashPin(20); 
+
 FlashPin(15); 
+FlashPin(14); 
 FlashPin(16); 
+FlashPin(10); 
 }
 
 void FlashPin(int pin)
